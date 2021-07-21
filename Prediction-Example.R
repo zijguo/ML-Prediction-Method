ec07e7743bf3b54f7a5f57092fd80e1081ab48a4
ywwes26 <49973874+ywwes26@users.noreply.github.com>
2021-07-21 08:27:42 -0400

    Update Prediction-Example.R

--- Prediction-Example.R
+++ Prediction-Example.R
@@ -1,53 +1,47 @@
 source("Prediction-Method.R", encoding = "UTF-8")
 
 # generating data
-n = 1000
-d = 1500
-X = 0.5*matrix(runif(n*d),n,d) + matrix(rep(0.5*runif(n),d),n,d)
+n = 1500
+p= 1500
+X = 0.5*matrix(runif(n*p),n,p) + matrix(rep(0.5*runif(n),p),n,p)
+colnames(X) <- paste("X", 1:p, sep = "")
 X4 <- X[,1:4] # for low dim
 
 # generating response
 Y = -2*sin(X[,1]) + X[,2]^2-1/3 + X[,3]-1/2 + exp(-X[,4])+exp(-1)-1 + rnorm(n)
 
+# train and test data
+test.ind <- sample(1:n,500)
+Y.test <- Y[test.ind]
+Y.train <- Y[-test.ind]
+X.test <- X[test.ind,]
+X.train <- X[-test.ind,]
+X4.test <- X4[test.ind,]
+X4.train <- X4[-test.ind,]
+
 
 # random forest
-# parameters
-num.trees = 500
-mtry = 1:NCOL(X4)
-max.depth = c(0,2,4,6)
-min.node.size = c(5,10,20)
-# model
-rf.fit <- random.forest(Y,X4,
-                        num.trees = num.trees,
-                        mtry = mtry,
-                        max.depth = max.depth,
-                        min.node.size = min.node.size)
+rf.fit <- random.forest(Y.train,X4.train)
 # MSE with full data, use Y[rf.fit$A1.ind] if data.split=TRUE
-mean((rf.fit$predicted.values-Y)^2)
+mean((rf.fit$predicted.values-Y.train)^2)
 # MSE using CV
 rf.fit$MSE.oob
+# test MSE
+Y.pred.rf <- predict(rf.fit$forest.model,data=X4.test,type="response")$predictions
+mean((Y.pred.rf-Y.test)^2)
+### Example to compute weight
+# weight <- RF.weight(rf.fit$nodes)
 
 
 # xgboost
-# parameters
-nrounds = 1000
-eta = c(0.05, 0.1, 0.3)
-max_depth = c(3, 6)
-min_child_weight = c(1, 3, 5)
-subsample = c(0.8, 1)
-colsample_bytree = c(0.8, 1)
-#model
-xgb.fit <- XGBoost(Y,X4,
-                   nrounds = nrounds,
-                   eta = eta,
-                   max_depth = max_depth,
-                   min_child_weight = min_child_weight,
-                   subsample = subsample,
-                   colsample_bytree = colsample_bytree)
+xgb.fit <- XGBoost(Y,X4)
 # MSE with full data, use Y[rf.fit$A1.ind] if data.split=TRUE
 mean((xgb.fit$predicted.values-Y)^2)
 # MSE using CV
 xgb.fit$MSE.cv
+# test MSE
+Y.pred.xgb <- predict(xgb.fit$xgb.model,newdata=X4.test)
+mean((Y.pred.xgb-Y.test)^2)
 
 
 # Sparse Additive Model
@@ -56,14 +50,17 @@ sam.fit <- SAM(Y,X4,high_dim = FALSE)
 mean((sam.fit$predicted.values-Y)^2)
 # MSE using CV
 sam.fit$MSE.cv
+# test MSE
+Y.pred.sam <- predict(sam.fit$sam.model,newdata=X4.test)$values
+mean((Y.pred.sam-Y.test)^2)
 
 
-# high-dimensional example for SAM
-sam.fit.hi <- SAM(Y,X)
-# MSE with full data, less overfitting
-mean((sam.fit.hi$predicted.values-Y)^2)
-# MSE using CV
-sam.fit.hi$MSE.cv
+# # high-dimensional example for SAM
+# sam.fit.hi <- SAM(Y,X)
+# # MSE with full data, less overfitting
+# mean((sam.fit.hi$predicted.values-Y)^2)
+# # MSE using CV
+# sam.fit.hi$MSE.cv
 
 
 
